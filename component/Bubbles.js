3d437ab84c67cb393d8df6d2ac97ee20398b1f9a
Waqar <waqar@chatspace.ai>
2024-09-12 17:02:52 +0100

    minor update

--- component/Bubbles.js
+++ component/Bubbles.js
@@ -210,14 +210,11 @@ function Bubbles(container, self, options) {
     bubble.appendChild(avatarWrap)
 
     // remove html tags
-
-    
-    bubbleWrapper.setAttribute("aria-label", "Chatbot said: " + adaptiveCardToText(card) + "\n Does this answer your question?")
-    bubbleWrapper.setAttribute("aria-role", "log")
-    bubbleWrapper.setAttribute("aria-live", "polite")
-    bubbleWrapper.setAttribute("aria-atomic", "true")
-    bubbleWrapper.setAttribute("tabindex", "0")
-
+    bubble.setAttribute("aria-label", "You said: " + text.replace(/<\/?[^>]+(>|$)/g, ""))
+    bubble.setAttribute("aria-role", "log")
+    bubble.setAttribute("aria-live", "polite")
+    bubble.setAttribute("aria-atomic", "true")
+    bubble.setAttribute("tabindex", "0")
 
     bubbleWrap.insertBefore(bubble, bubbleTyping)
     containerHeight = container.offsetHeight
